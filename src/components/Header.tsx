bc84c534ea457d28df8f310638e566721af6fc1d
Lucas Castro <lucasamonrc@gmail.com>
2021-04-09 18:55:12 -0600

    feature: add active link component to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+import ActiveLink from './ActiveLink';
+
 import common from '../styles/common.module.scss';
 import styles from '../styles/components/header.module.scss';
 
@@ -11,18 +13,18 @@ export default function Header() {
           <a><img src="/logo.svg" alt="Lucas Castro"/></a>
         </Link>
         <nav>
-          <Link href="/about">
+          <ActiveLink activeClassName={styles.active} href="/about">
             <a>About</a>
-          </Link>
-          <Link href="/projects">
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/projects">
             <a>Projects</a>
-          </Link>
-          <Link href="/journal">
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/journal">
             <a>Journal</a>
-          </Link>
-          <Link href="https://rb.gy/evmrcf">
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="https://rb.gy/evmrcf">
             <a>Resume</a>
-          </Link>
+          </ActiveLink>
         </nav>
       </div>
     </header>
