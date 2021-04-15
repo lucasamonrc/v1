f340696f2be2532088d3cea1623f16f3b9d39bd1
Lucas Castro <lucasamonrc@gmail.com>
2021-04-14 22:16:45 -0600

    add journal page

---
+++ src/pages/about.tsx
@@ -0,0 +1,94 @@
+import Head from 'next/head';
+
+import JournalEntry from '../components/JournalEntry';
+
+import common from '../styles/common.module.scss';
+import styles from '../styles/pages/about.module.scss';
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>Home | Lucas A. Castro</title>
+      </Head>
+
+      <main className={common.content}>
+        <section className={styles.journal}>
+          <h1>Journal</h1>
+
+          <JournalEntry 
+            title='How to use React Hooks'
+            summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna sed sapien interdum sed.'
+            publicationDate='Apr 9, 2021'
+            readingTime='4'
+          />
+          
+          <JournalEntry 
+            title='How to use React Hooks'
+            summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna sed sapien interdum sed.'
+            publicationDate='Apr 9, 2021'
+            readingTime='4'
+          />
+          
+          <JournalEntry 
+            title='How to use React Hooks'
+            summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna sed sapien interdum sed.'
+            publicationDate='Apr 9, 2021'
+            readingTime='4'
+          />
+
+          <JournalEntry 
+            title='How to use React Hooks'
+            summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna sed sapien interdum sed.'
+            publicationDate='Apr 9, 2021'
+            readingTime='4'
+          />
+          
+          <JournalEntry 
+            title='How to use React Hooks'
+            summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna sed sapien interdum sed.'
+            publicationDate='Apr 9, 2021'
+            readingTime='4'
+          />
+
+          <JournalEntry 
+            title='How to use React Hooks'
+            summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna sed sapien interdum sed.'
+            publicationDate='Apr 9, 2021'
+            readingTime='4'
+          />
+          
+          <JournalEntry 
+            title='How to use React Hooks'
+            summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna sed sapien interdum sed.'
+            publicationDate='Apr 9, 2021'
+            readingTime='4'
+          />
+          
+          <JournalEntry 
+            title='How to use React Hooks'
+            summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna sed sapien interdum sed.'
+            publicationDate='Apr 9, 2021'
+            readingTime='4'
+          />
+          
+          <JournalEntry 
+            title='How to use React Hooks'
+            summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna sed sapien interdum sed.'
+            publicationDate='Apr 9, 2021'
+            readingTime='4'
+          />
+          
+          <JournalEntry 
+            title='How to use React Hooks'
+            summary='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna sed sapien interdum sed.'
+            publicationDate='Apr 9, 2021'
+            readingTime='4'
+          />
+
+          <button type="button">Load more entries</button>
+        </section>
+      </main>
+    </>
+  );
+}
